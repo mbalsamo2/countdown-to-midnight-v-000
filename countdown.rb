eecd98923c6b6c0dbbe98042f2ffec54258e5d64
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-19 15:26:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,10 @@ def countdown(integer)
 end
 
 def countdown_with_sleep(integer)
-  sleep 5
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
